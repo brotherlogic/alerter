066557996444676be6c39b1bbd689de25ba90817
Simon Tucker <brotherlogic@gmail.com>
2018-09-30 10:16:24 -0700

    Adds alert on bad version.

--- alerterutils.go
+++ alerterutils.go
@@ -1,5 +1,32 @@
 package main
 
-func run() {
-	//pass
+import (
+	pbbs "github.com/brotherlogic/buildserver/proto"
+	"golang.org/x/net/context"
+
+	pbgs "github.com/brotherlogic/gobuildslave/proto"
+
+	pbd "github.com/brotherlogic/discovery/proto"
+)
+
+func (s *Server) runVersionCheck(ctx context.Context) {
+	serv, err := s.discover.ListAllServices(ctx, &pbd.ListRequest{})
+	if err == nil {
+		for _, service := range serv.Services.Services {
+			if service.Name == "gobuildslave" {
+				jobs, err := s.gobuildSlave.ListJobs(ctx, service, &pbgs.ListRequest{})
+				if err == nil {
+					for _, job := range jobs.Jobs {
+						runningVersion := job.RunningVersion
+						latest, err := s.buildServer.GetVersions(ctx, &pbbs.VersionRequest{Job: job.Job, JustLatest: true})
+
+						if err == nil && len(latest.Versions) > 0 && latest.Versions[0].Version != runningVersion {
+							s.alertCount++
+							s.RaiseIssue(ctx, "Version Problem", "Version", false)
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- alerterutils_test.go
+++ alerterutils_test.go
@@ -1,7 +1,49 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestCoverage(t *testing.T) {
-	run()
+	"github.com/brotherlogic/keystore/client"
+	"golang.org/x/net/context"
+
+	pbbs "github.com/brotherlogic/buildserver/proto"
+	pbd "github.com/brotherlogic/discovery/proto"
+	pbgbs "github.com/brotherlogic/gobuildslave/proto"
+)
+
+type testDiscovery struct{}
+
+func (t *testDiscovery) ListAllServices(ctx context.Context, req *pbd.ListRequest) (*pbd.ListResponse, error) {
+	return &pbd.ListResponse{Services: &pbd.ServiceList{Services: []*pbd.RegistryEntry{&pbd.RegistryEntry{Name: "gobuildslave", Ip: "1234", Port: int32(123)}}}}, nil
+}
+
+type testBuildserver struct{}
+
+func (t *testBuildserver) GetVersions(ctx context.Context, req *pbbs.VersionRequest) (*pbbs.VersionResponse, error) {
+	return &pbbs.VersionResponse{Versions: []*pbbs.Version{&pbbs.Version{Version: "testing"}}}, nil
+}
+
+type testGobuildslave struct{}
+
+func (t *testGobuildslave) ListJobs(ctx context.Context, server *pbd.RegistryEntry, req *pbgbs.ListRequest) (*pbgbs.ListResponse, error) {
+	return &pbgbs.ListResponse{Jobs: []*pbgbs.JobAssignment{&pbgbs.JobAssignment{RunningVersion: "not_testing"}}}, nil
+}
+
+func InitTestServer() *Server {
+	s := Init()
+	s.discover = &testDiscovery{}
+	s.buildServer = &testBuildserver{}
+	s.gobuildSlave = &testGobuildslave{}
+	s.SkipLog = true
+	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
+	return s
+}
+
+func TestAlert(t *testing.T) {
+	s := InitTestServer()
+	s.runVersionCheck(context.Background())
+
+	if s.alertCount != 1 {
+		t.Errorf("Error in alerting")
+	}
 }

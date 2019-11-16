9afed947dee8d609d1d7b02162a3e04a90c2a0d9
Simon Tucker <simon@brotherlogic@gmail>
2019-11-15 19:31:02 -0800

    Thing

--- alerter.go
+++ alerter.go
@@ -157,7 +157,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "blah", Value: int64(100)},
+	}
 }
 
 func (s *Server) runVersionCheckLoop(ctx context.Context) error {
